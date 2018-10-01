d0814232041ac4341edf9af0448d0256f977636a
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2018-10-01 21:35:54 +0200

    Create myApp.js

---
+++ myApp.js
@@ -0,0 +1,12 @@
+
+var express = require('express');
+var app = express();
+
+// --> 7)  Mount the Logger middleware here
+
+
+// --> 11)  Mount the body-parser middleware  here
+
+
+/** 1) Meet the node console. */
+console.log("Hello World");
