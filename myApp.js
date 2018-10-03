ccaa1c500c8b4315511a955f77d0ad5ac73b2817
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2018-10-03 20:26:26 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,22 +3,32 @@ var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(function (req, res, next){
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  
+  next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 /** 1) Meet the node console. */
 console.log("Hello World");
 
-app.get("/", function(req, res){
+/** 2) A first working Express Server */
+/*app.get("/", function(req, res){
   res.send("Hello Express");
-});
+});*/
 
-app.get("/", function(req, res){
+/** 3) Serve an HTML file */
+
+app.get("/", function( req, res){
   res.sendFile(__dirname + "/views/index.html");
 });
-
+/** 4) Serve static assets  */
 app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
@@ -29,3 +39,49 @@ app.get("/json", function(req, res){
   res.json({"message": "Hello json"});
  }
 });
+
+/** 6) Use the .env file to configure the app */
+ 
+ 
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+
+
+/** 8) Chaining middleware. A Time server */
+app.get("/now", function(req, res, next){
+  req.time = new Date().toString();
+  
+  next();
+}, function(req, res){
+  console.log(req.time);
+  res.json({"time": req.time});
+});
+
+/** 9)  Get input from client - Route parameters */
+app.get("/:word/echo", function(req, res){
+  res.json({"echo": req.params.word});
+});
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+app.get('/name', (req, res) => {
+  let jsonObj = {name: `${req.query.first} ${req.query.last}` };
+  res.send(jsonObj);
+});
+  
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+
+/** 12) Get data form POST  */
+
+
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+ module.exports = app;
